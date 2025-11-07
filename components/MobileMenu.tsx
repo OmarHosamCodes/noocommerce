ad9fe58fabe74ea31a4aae5e7ea7b6873c1f31c3
Ashir Afridi <ashirafridi.work@gmail.com>
2025-11-08 00:03:22 +0500

    fix: mobile menu minor

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 
 const navItems = [
     { name: "Home", href: "/" },
+    { name: "Shop", href: "/shop" },
     { name: "About", href: "/about" },
     { name: "Contact", href: "/contact" },
 ];
